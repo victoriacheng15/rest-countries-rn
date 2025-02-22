161c0661b917c43c22a94662985dd242ef8d971a
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-22 12:31:42 -0700

    chore: add input element

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,34 @@
-import { StyleSheet, View, Text } from "react-native";
-import { useNavigation } from "expo-router";
-import { useEffect } from "react";
-import { Link } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput } from "react-native-paper";
+import { COLORS } from "@/utils";
 
 export default function App() {
-	const navigation = useNavigation();
+  const [search, setSearch] = useState("");
 
-	useEffect(() => {
-		navigation.setOptions({ headerShown: false });
-	}, [navigation]);
-
-	return (
-		<View style={styles.container}>
-			<Text>Hello World</Text>
-			<Link href="/about">About</Link>
-			<Link href="/country/Canada">Canada Detailed Page</Link>
-		</View>
-	);
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <TextInput
+          mode="outlined"
+					activeOutlineColor={COLORS.primary}
+          label="Search by country name"
+          value={search}
+          onChangeText={(text) => setSearch(text)}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  safeArea: {
+    flex: 1,
+    backgroundColor: COLORS.tertiary,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
 });
