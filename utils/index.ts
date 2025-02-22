2e8735d650395ab8ae6c9d26a50e061b4be3700d
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-22 12:31:10 -0700

    chore: add colors obj

--- utils/index.ts
+++ utils/index.ts
@@ -8,6 +8,12 @@ const SET_LOADING = "SET_LOADING";
 const FETCH_SUCCESS = "FETCH_SUCCESS";
 const FETCH_ERROR = "FETCH_ERROR";
 
+const COLORS = {
+	primary: "#14213D",
+	secondary: "#FCA311",
+	tertiary: "#E5E5E5",
+};
+
 export {
 	API_COUNTRY_LIST,
 	API_COOUNTRY,
@@ -15,4 +21,5 @@ export {
 	FETCH_SUCCESS,
 	FETCH_ERROR,
 	countryListReducer,
+	COLORS,
 };
