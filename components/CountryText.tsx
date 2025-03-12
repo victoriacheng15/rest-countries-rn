6764a9557a24fc14780fa65b4af9e3db47353365
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-11 18:44:39 -0600

    refactor: contnet can be either string or string array

--- components/CountryText.tsx
+++ components/CountryText.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, View, Text } from "react-native";
 
 interface CountryTextProps {
 	contentTitle: string;
-	content: string;
+	content: string | string[];
 }
 
 export default function CountryText({
@@ -12,20 +12,35 @@ export default function CountryText({
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>{contentTitle}</Text>
-			<Text style={styles.info}>{content}</Text>
+			{typeof content === "string" ? (
+				<Text style={[styles.info, styles.moveLeft]}>{content}</Text>
+			) : (
+				<View style={styles.moveLeft}>
+					{content.map((item) => (
+						<Text key={item} style={styles.info}>
+							{item}
+						</Text>
+					))}
+				</View>
+			)}
 		</View>
 	);
 }
 const styles = StyleSheet.create({
 	container: {
-		flexDirection: "row",
-		alignItems: "center",
+		flex: 1,
+		gap: 2,
+		marginBottom: 8,
 	},
 	title: {
-		fontSize: 16,
+		fontSize: 18,
 		fontWeight: "bold",
 	},
 	info: {
 		fontSize: 16,
+	},
+	moveLeft: {
+		marginLeft: 16,
+		gap: 4,
 	},
 });
